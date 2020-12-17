745dad70b7d42a7364cc5e53baa9cece3d04a9fc
alphabotshero <alphabotshero@gmail.com>
2020-12-17 19:57:20 +0530

    updated typos in leave and help

--- commands/leave.js
+++ commands/leave.js
@@ -6,7 +6,27 @@ module.exports = {
         aliases: ["l","dc","disconnect"]
     },
     run: async (client, message, args) => {
-        
+        if (!message.member.voice.channel) return message.channel.send({
+            embed: {
+                color: '#FA1D2F',
+                description: `You have to be connected to a voice channel before you can use this command.`,
+            },
+        });
+
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send ({
+            embed: {
+                color: '#FA1D2F',
+                description: 'Oh-oh! we are not in same voice channel'
+            }
+        })
+
+        if (message.guild.me.voice.channel && message.member.voice.channel.id === message.guild.me.voice.channel.id) return message.channel.send ({
+            embed: {
+                color: '#FA1D2F',
+                description: 'Iam already being used in another channel.'
+            }
+        })
+
      message.member.voice.channel.leave();
 
         message.channel.send ({
