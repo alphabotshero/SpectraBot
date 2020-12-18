88a47e72268f3d73d54c5621f370196c4c31af09
alphabotshero <alphabotshero@gmail.com>
2020-12-19 00:35:40 +0530

    ok

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -12,52 +12,14 @@ module.exports = {
             .setThumbnail(client.user.displayAvatarURL())
             .setTitle('Bot Stats')
             .setColor('#00ffff')
-            .addFields(
-                {
-                    name: 'Bot Version',
-                    value: `0.3`,
-                    inline: true
-                },
-                {
-                    name: 'node.js version',
-                    value: `14.15.1`,
-                    inline: true
-                },
-                {
-                    name: 'Discord.js version',
-                    value: `3.0.0`,
-                    inline: true
-                },
-                {
-                    name: 'Servers',
-                    value: `${client.guilds.cache.size}`,
-                    inline: true
-                },
-                {
-                    name: 'Channels',
-                    value: `${client.channels.cache.size}`,
-                    inline: true
-                },
-                {
-                    name: 'Users',
-                    value: `${client.users.cache.size}`,
-                    inline: true
-                },
-                {
-                    name: 'Ping',
-                    value: `${Math.round(client.ws.ping)}ms`,
-                    inline: true
-                },
-                {
-                    name: 'Connections',
-                    value: `${client.voice.connections.size}`,
-                    inline: true
-                },
-                {
-                    name: 'Server Info',
-                    value: `Cores: ${os.cpus().length}`,
-                    inline: true
-                })
+            .addField("Bot Version",`0.3` ,true)
+            .addField("Node.js Version",`12.3.1` ,true)
+            .addField("Discord.js Version",`3.0.0` ,true)
+            .addField("Servers",`${client.guilds.cache.size}` ,true)
+            .addField("Channels",`${client.channels.cache.size}` ,true)
+            .addField("Users",`${client.users.cache.size}` ,true)
+            .addField("Ping",`${client.ws.ping}ms` ,true)
+            .addField("Connections",`${client.voice.connections.size}` ,true)
             .setFooter(`Created By ${message.author.tag}`, message.author.displayAvatarURL())
 
         await message.channel.send(embed)
--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Beta V2.0 Build version", {type: "WATCHING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+  await client.user.setActivity("Beta V2.0 Build version", {type: "PLAYING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
