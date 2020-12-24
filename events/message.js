6506b05fd632319df22d0688efe3e005cea9c492
alphabotshero <alphabotshero@gmail.com>
2020-12-24 11:29:33 +0530

    Updated Status

--- events/message.js
+++ events/message.js
@@ -1,22 +1,15 @@
 module.exports = async (client, message) => {
   if (message.author.bot) return;
 
-  //Prefixes also have mention match
   const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
   const prefix = message.content.match(prefixMention) ? message.content.match(prefixMention)[0] : client.config.prefix;
 
   if (message.content.indexOf(prefix) !== 0) return;
 
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  //Making the command lowerCase because our file name will be in lowerCase
   const command = args.shift().toLowerCase();
-
-  //Searching a command
   const cmd = client.commands.get(command);
-  //Searching a command aliases
   const aliases = client.commands.find(x => x.info.aliases.includes(command))
-
-  //if(message.channel.type === "dm")return message.channel.send("None of the commands work in DMs. So please use commands in server!")
 process.on("unhandledRejection", (reason, promise) => {
     try {
         console.error("Unhandled Rejection at: ", promise, "reason: ", reason.stack || reason);
@@ -26,8 +19,6 @@ process.on("unhandledRejection", (reason, promise) => {
 });
 require('events').EventEmitter.defaultMaxListeners = 100000000000000000000000000000
 
-
-  //Executing the codes when we get the command or aliases
   if(cmd){
     cmd.run(client, message, args);
   }else if(aliases){
--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("s!help and s!play", {type: "PLAYING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+  await client.user.setActivity("YOUTUBE PLAYLISTS | s!help ", {type: "WATCHING",
   });
 };
